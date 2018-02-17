dd1071dcf37aa0524ebe4be0186316c812857dc3
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-02-17 20:53:02 -0300

    aula 13: criando a view Home; elementos básicos. - Issue #11

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,7 +3,14 @@
 namespace App\Controllers;
 
 class HomeController {
+    private $view;
+
+    public function __construct() {
+        $this->view = new \stdClass;
+    }
+
     public function index() {
-        echo "Home";
+        $this->view->nome = "Valdinei Ferreira";
+        require_once __DIR__ . "/../Views/home/index.phtml";
     }
 }
