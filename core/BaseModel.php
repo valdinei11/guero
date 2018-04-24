83f28e2651c22cfd0f27f7d081cc6847288e2083
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-24 19:59:52 -0300

    BaseModel adicionada

---
+++ core/BaseModel.php
@@ -0,0 +1,22 @@
+<?php
+namespace Core;
+
+use PDO;
+
+abstract class BaseModel {
+    private $pdo;
+    protected $table;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function all() {
+        $query = "SELECT * FROM {$this->table}";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        $stmt->closeCursor();
+        return $result;
+    }
+}
