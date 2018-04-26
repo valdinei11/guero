9fd2f442c6425c9a213a1654b00344059f99038e
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-26 14:28:50 -0300

    refazendo estilização da página inicial: padding reduzido

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -7,8 +7,8 @@
         <p>Dentre as diversas vantagens de utilizar o Guêro, está a sua agilidade em seus serviços, concluindo as dezenas de atividades complexas em pouco tempo, economizando tempo para o desenvolvedor.</p>
     </div>
     <div class="flex-col">
-        <h2>Come no Cantinho</h2>
-        <p>Um framework que faz o que se propõe e só lhe incomoda quando realmente necessário.</p>
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <div class="flex-col">
         <h2>e aí, vamo fecha?</h2>
