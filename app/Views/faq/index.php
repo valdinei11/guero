38d48fbd6fba196cdb28e4a26fb037ce6102e73d
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-27 14:52:06 -0300

    estilização da página de FAQ

--- app/Views/faq/index.php
+++ app/Views/faq/index.php
@@ -2,7 +2,11 @@
     <h1><?php $this->getPageTitle(); ?></h1>
     <div>
         <?php foreach ($this->view->faq as $faq):?>
-            <a href="/faq/<?= $faq->id ?>/show"><h2><?= $faq->question ?></h2></a>
+            <div class="float-btn">
+                <a class="btn btn-success" href="faq/<?= $faq->id ?>/edit">Editar</a>
+                <a class="btn btn-danger" href="faq/<?= $faq->id ?>/delete">Apagar</a>
+            </div>
+            <h2 class="underline"><a href="/faq/<?= $faq->id ?>/show"><?= $faq->question ?></a></h2>
             <h3><?= $faq->content ?></h3>
         <?php endforeach; ?>
     </div>
