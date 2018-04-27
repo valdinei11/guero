5f7e3fc66b480904c2a4d7f99e6426ca1f7859aa
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-27 15:43:09 -0300

    edição de botões

--- app/Views/faq/index.php
+++ app/Views/faq/index.php
@@ -3,7 +3,7 @@
     <div>
         <?php foreach ($this->view->faq as $faq):?>
             <div class="float-btn">
-                <a class="btn btn-success" href="faq/<?= $faq->id ?>/edit">Editar</a>
+                <a class="btn btn-primary" href="faq/<?= $faq->id ?>/edit">Editar</a>
                 <a class="btn btn-danger" href="faq/<?= $faq->id ?>/delete">Apagar</a>
             </div>
             <h2 class="underline"><a href="/faq/<?= $faq->id ?>/show"><?= $faq->question ?></a></h2>
