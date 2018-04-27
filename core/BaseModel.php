2664bd982a1b003acb92e9cababacfa48a257c49
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-27 13:03:43 -0300

    método find adicionado a BaseModel

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -19,4 +19,14 @@ abstract class BaseModel {
         $stmt->closeCursor();
         return $result;
     }
+
+    public function find($id) {
+        $query = "SELECT * FROM {$this->table} WHERE id=:id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindValue(":id", $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        $stmt->closeCursor();
+        return result;
+    }
 }
