1cf244f22032854ecc4d9598d8ae222e346a1cba
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-29 23:16:25 -0300

    mudança nas margens de divs e containers em geral

--- app/Views/faq/index.php
+++ app/Views/faq/index.php
@@ -2,14 +2,18 @@
     <h1><?php $this->getPageTitle(); ?></h1>
 </div>
 <section>
-    <div>
+    <div class="container">
         <?php foreach ($this->view->faq as $faq):?>
-            <div class="float-btn">
-                <a class="btn btn-primary" href="faq/<?= $faq->id ?>/edit">Editar</a>
-                <a class="btn btn-danger" href="faq/<?= $faq->id ?>/delete">Apagar</a>
+        <div>
+            <div class="box-header">
+                <h2 class="underline"><a href="/faq/<?= $faq->id ?>/show"><?= $faq->question ?></a></h2>
+                <div class="float-btn">
+                    <a class="btn btn-primary" href="faq/<?= $faq->id ?>/edit">Editar</a>
+                    <a class="btn btn-danger" href="faq/<?= $faq->id ?>/delete">Apagar</a>
+                </div>
             </div>
-            <h2 class="underline"><a href="/faq/<?= $faq->id ?>/show"><?= $faq->question ?></a></h2>
-            <h3><?= $faq->content ?></h3>
+            <p class="txt-big"><?= $faq->content ?></h3>
+        </div>
         <?php endforeach; ?>
     </div>
 </section>
