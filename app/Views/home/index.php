0ed755779efc12287e5f6cb40042a0792f26be08
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-29 22:03:44 -0300

    texto da index modificado

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -3,12 +3,12 @@
 </section>
 <section class="flexbox">
     <div class="flex-col">
-        <h2>R A P I D A M E N T E</h2>
-        <p>Dentre as diversas vantagens de utilizar o Guêro, está a sua agilidade em seus serviços, concluindo as dezenas de atividades complexas em pouco tempo, economizando tempo para o desenvolvedor.</p>
+        <h2> Lorem Ipsum </h2>
+        <p>Pellentesque dictum facilisis odio, eu laoreet mauris ullamcorper nec. Donec a massa vel nisi eleifend viverra nec sed enim. Suspendisse non erat vitae nisl aliquam pulvinar ut non risus. Morbi pulvinar, ligula id interdum iaculis, lacus metus condimentum diam, auctor eleifend dolor erat quis ex.</p>
     </div>
     <div class="flex-col">
         <h2>Lorem Ipsum</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     </div>
     <div class="flex-col">
         <h2>e aí, vamo fecha?</h2>
